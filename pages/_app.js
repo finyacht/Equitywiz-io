f76f42c6b6d79e7a9cdc1f38dbf532572b289b5b
finyacht <zk72kk@gmail.com>
2025-07-23 16:56:58 +0100

    Fix Netlify plugin issue: Remove NextJS artifacts and configure for Express.js deployment

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../src/styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-  return React.createElement(Component, pageProps);
-}
-
-export default MyApp; 
